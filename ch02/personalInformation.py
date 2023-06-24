cc52d9b7c1f5589895f0b3d7dd837aa09905ed91
outHereSam <larbisamuel0@gmail.com>
2023-06-24 14:05:57 +0000

    Initial commit

---
+++ ch02/personalInformation.py
@@ -0,0 +1,10 @@
+# Write a program that displays the following information:
+# • Your name
+# • Your address, with city, state, and ZIP
+# • Your telephone number
+# • Your college major
+
+print('Samuel Larbi Danquah')
+print('Yummy Cl, Apollo, Takoradi')
+print('0248185717')
+print('Computer Science')
