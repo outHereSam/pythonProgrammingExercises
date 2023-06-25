6f2310c8dd3666d9cbbfd2f9a31066aac0b0c2f7
outHereSam <larbisamuel0@gmail.com>
2023-06-25 22:28:39 +0000

    Completes main chapter 2 exercises

---
+++ ch02/plantingGrapevines.py
@@ -0,0 +1,28 @@
+# A vineyard owner is planting several new rows of grapevines, and needs to know how many
+# grapevines to plant in each row. She has determined that after measuring the length of a
+# future row, she can use the following formula to calculate the number of vines that will fit
+# in the row, along with the trellis end-post assemblies that will need to be constructed at
+# each end of the row:
+# V = R - 2E / S
+# The terms in the formula are:
+# V is the number of grapevines that will fit in the row.
+# R is the length of the row, in feet.
+# E is the amount of space, in feet, used by an end-post assembly.
+# S is the space between vines, in feet.
+# Write a program that makes the calculation for the vineyard owner. The program should
+# ask the user to input the following:
+# • The length of the row, in feet
+# • The amount of space used by an end-post assembly, in feet
+# • The amount of space between the vines, in feet
+# Once the input data has been entered, the program should calculate and display the num-
+# ber of grapevines that will fit in the row.
+
+length = float(input("Enter the length of the row (in feet): "))
+endPostSpace = float(
+    input("Enter the amount of space used by an end-post assembly (in feet): ")
+)
+vineSpace = float(input("Enter the amount of space between the vines (in feet): "))
+
+numGrapeVinesInRow = (length - (2 * endPostSpace)) / vineSpace
+
+print(f"The number of grapevines that will fit in the row: {numGrapeVinesInRow:.2f}")
